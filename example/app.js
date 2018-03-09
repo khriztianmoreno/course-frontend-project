476edf5226f1834cd720c4523f03fbd89c0812fc
khriztianmoreno <khriztianmoreno@gmail.com>
2018-03-08 20:47:57 -0500

    Add validate age before save

--- example/app.js
+++ example/app.js
@@ -5,6 +5,9 @@ const $inputEmail = document.getElementById('email')
 const $inputAge = document.getElementById('age')
 const $userList = document.getElementById('usersList')
 
+// Nuevo
+const $inputDrink = document.getElementById('drink')
+
 function saveUser(evt) {
   // Evitar que se recargue
   evt.preventDefault();
@@ -12,19 +15,41 @@ function saveUser(evt) {
   const name = $inputName.value
   const email = $inputEmail.value
   const age = $inputAge.value
-
   // Nuevo
+  const drink = $inputDrink.value
+
+
   const $node = document.createElement('tr');
 
   const row = `
   <tr>
-    <td>1</td>
+    <td>${drink}</td>
     <td>${name}</td>
     <td>${email}</td>
     <td>${age}</td>
   </tr>
   `
   // Nuevo
-  $node.innerHTML = row;
-  $userList.appendChild($node)
+  const validate = validateAge()
+
+  if (validate) {
+    $node.innerHTML = row;
+    $userList.appendChild($node)
+  }
+
+}
+
+// Nuevo
+function validateAge() {
+  // const drink = $inputDrink.value
+  const optionIndexSelected = $inputDrink.selectedIndex
+  const drink = $inputDrink.options[optionIndexSelected].value
+  const age = $inputAge.value
+  
+  if (age < 18 && drink !== 'Agua') {
+    alert('Muy bello, pero no')
+    return false
+  }
+
+  return true
 }
