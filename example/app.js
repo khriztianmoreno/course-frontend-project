809512fd76cacd6247d303787a1f74980324f5bd
khriztianmoreno <khriztianmoreno@gmail.com>
2018-03-09 20:41:22 -0500

    Example firebase

--- example/app.js
+++ example/app.js
@@ -1,12 +1,24 @@
 'use strict';
 
+// NUEVO
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDIjG86wPeDpDm7EwtxvOa6kHplTlwDaW0",
+  authDomain: "fir-front.firebaseapp.com",
+  databaseURL: "https://fir-front.firebaseio.com",
+  projectId: "fir-front",
+  storageBucket: "fir-front.appspot.com",
+  messagingSenderId: "1092284647709"
+};
+firebase.initializeApp(config);
+
 const $inputName = document.getElementById('name')
 const $inputEmail = document.getElementById('email')
 const $inputAge = document.getElementById('age')
 const $userList = document.getElementById('usersList')
-
-// Nuevo
 const $inputDrink = document.getElementById('drink')
+// NUEVO
+const database = firebase.database()
 
 function saveUser(evt) {
   // Evitar que se recargue
@@ -15,27 +27,19 @@ function saveUser(evt) {
   const name = $inputName.value
   const email = $inputEmail.value
   const age = $inputAge.value
-  // Nuevo
   const drink = $inputDrink.value
 
-
-  const $node = document.createElement('tr');
-
-  const row = `
-  <tr>
-    <td>${drink}</td>
-    <td>${name}</td>
-    <td>${email}</td>
-    <td>${age}</td>
-  </tr>
-  `
-  // Nuevo
-  const validate = validateAge()
-
-  if (validate) {
-    $node.innerHTML = row;
-    $userList.appendChild($node)
+  // NUEVO
+  const id = Math.floor(Math.random() * 3333333)
+  // ES6
+  const user = {
+    name,
+    email,
+    age,
+    drink
   }
+  debugger
+  database.ref(`users/${id}`).set(user)
 
 }
 
