bd36b9e18cee5410383b5c065d24d66d2415a2f0
khriztianmoreno <khriztianmoreno@gmail.com>
2018-03-16 20:43:01 -0500

    Intregrate HTML with Javascript Only one employee

--- algorithm/app.js
+++ algorithm/app.js
@@ -20,12 +20,23 @@ const RTF = 0.125
 
 const valRTF = SALARY * RTF
 
+const $employeeNode = document.getElementById('employee')
+
+let comBaseEmployee1 = 0
+let comGoldEmployee1 = 0
+
+let comBaseEmployee2 = 0
+let comGoldEmployee2 = 0
+
+let comBaseEmployee3 = 0
+let comGoldEmployee3 = 0
+
 function calcCommisionBase(productVal) {
-  let valComPlus
+  let valComPlus = 0
   let valComGold = 0
   // Calcular el valor de la comision general
   const valComBase = productVal * COMMISSSION_BASE
-  if (productVal > 100000) {
+  if (productVal >= 100000) {
     // Calcular el valor de la comision PLUS
     valComPlus = productVal * COMMISSSION_PLUS
   }
@@ -44,7 +55,36 @@ function calcularComisionGold(totalVentas) {
   return valComGold
 }
 
-const productVal = prompt('El valor del product:')
+function registerSale(evt) {
+  evt.preventDefault()
+  const employee = $employeeNode.value
+  const productVal = document.getElementById('product').value
+  
+  if (employee === '1') {
+    comBaseEmployee1 = comBaseEmployee1 + calcCommisionBase(productVal)
+    comGoldEmployee1 = comGoldEmployee1 + calcularComisionGold(productVal)
+  }
+
+  if (employee === '2') {
+    comBaseEmployee2 = comBaseEmployee2 + calcCommisionBase(productVal)
+    comGoldEmployee2 = comGoldEmployee2 + calcularComisionGold(productVal)
+  }
+
+  if (employee === '3') {
+    comBaseEmployee3 = comBaseEmployee3 + calcCommisionBase(productVal)
+    comGoldEmployee3 = comGoldEmployee3 + calcularComisionGold(productVal)
+  }
+}
+
+function closeDay () {
+  const totalGanacia1 = comBaseEmployee1 + comGoldEmployee1
+  const totalAPagar1 = (SALARY + totalGanacia1 )
+
+  console.log('Comision: ', totalGanacia1)
+  console.log('Salario: ', totalAPagar1)
+}
+
+/*const productVal = prompt('El valor del product:')
 const totalEmploye1 = calcCommisionBase(productVal)
 
 const productVal2 = prompt('El valor del product:')
@@ -60,3 +100,4 @@ const totalGanacia2 = totalEmploye2 + comGoldEmployee2
 
 const totalAPagar1 = (SALARY + totalGanacia1 ) - valRTF
 const totalAPagar2 = (SALARY + totalGanacia2 ) - valRTF
+*/
